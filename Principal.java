21f206a87127c2bd27dfa3a8a6c14d88369b7ff9
luanaraujo2 <luanzinho.ao05@gmail.com>
2026-02-23 20:40:26 -0300

    Add files via upload

--- Principal.java
+++ Principal.java
@@ -1,61 +1,26 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package com.mycompany.principal;
 
 /**
  *
  * @author alunolab01
  */
-public class Conta {
-    private double saldo, limite;
-    private int agencia, numero;
-    
-    double getSaldo(){
-        return saldo;
-    }
-    
-    void setAgencia(int agencia){
-        this.agencia = agencia;
-    }
-    
-    int getAgencia(){
-        return agencia;
-    }
-    
-    void setNumero(int numero){
-        this.numero = numero;
-    }
-    
-    public Conta(){
-    
-    }
-    
-    public Conta(int agencia, int numero, double valor){
-        this.agencia = agencia;
-        this.numero = numero;
-        saldo = saldo + valor;
-    }
-    
-    void creditar(double valor){
-        saldo = saldo + valor;
-    }
-    
-    void debitar(double valor){
-        double saldoTotal;
-        saldoTotal = saldo + limite; 
-        if (valor <= saldoTotal){
-        saldo = saldo - valor;
-        }
-    }
-    
-    void transferir(Conta destino,double valor){
-        debitar(valor);
-        destino.creditar(valor);
+public class Principal {
+
+    public static void main(String[] args) {
+        Conta c1 = new Conta(123, 6630, 5000);
+        Conta c2 = new Conta();
+        c2.setAgencia(124);
+        c2.setNumero(6631);
+        c2.creditar(15000);
+  
+        c1.transferir(c2, 4000);
+        
+        System.out.println(c1.getSaldo());
+        System.out.println(c2.getSaldo());
         
     }
-    
-    }
-    
-   
+}
